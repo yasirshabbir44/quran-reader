3149a80664797d1b333346a51ad4da005feafaf5
Yasir Shabbir <yasirshabbir44@gmail.com>
2026-05-04 01:52:37 +0400

    Enhance MulkReaderComponent with Quran data loading and improved routing
    - Added a new script command in package.json for building Quran data.
    - Integrated HTTP client in app configuration for data fetching.
    - Updated routing to allow direct navigation to specific Surahs and handle invalid routes.
    - Enhanced MulkReaderComponent to manage Quran data loading states and errors, improving user experience.
    - Updated HTML structure to reflect loading states and error messages, ensuring better accessibility and user feedback.
    - Localized document titles and error messages for improved internationalization support.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { MulkReaderComponent } from './mulk-reader/mulk-reader.component';
 
-export const routes: Routes = [{ path: '', component: MulkReaderComponent }];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'surah/67' },
+  { path: 'surah/:n', component: MulkReaderComponent },
+  { path: '**', redirectTo: 'surah/67' },
+];
